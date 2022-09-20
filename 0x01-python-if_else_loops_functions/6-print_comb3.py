e748f4a743336868050bdb740ad18f75e78ad560
BestStar42 <rajijamiu968@gmail.com>
2022-09-20 13:07:06 -0700

    fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 90):
-        if num % 10 > num / 10:
-                if num != 89:
-                        print("{:02d}, ".format(num), end='')
-                else:
-                        print("{:02d}".format(num))
+for i in range(48, 58):
+    for j in range(48, 58):
+        if j > i & i < 56:
+            print('{}{}'.format(chr(i), chr(j)), end=', ')
+print('{:d}'.format(89))
